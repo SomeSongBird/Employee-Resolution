534692532fe771cd14df9870221e02f395fa4a72
SomeStupidSongBird <563ftw@gmail.com>
2022-10-23 23:45:50 -0500

    Setup title screen

--- app/src/main/java/com/songbirdnest/employee_resolution/TitleActivity.kt
+++ app/src/main/java/com/songbirdnest/employee_resolution/TitleActivity.kt
@@ -1,11 +1,29 @@
 package com.songbirdnest.employee_resolution
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import androidx.constraintlayout.widget.ConstraintSet.Motion
+import androidx.core.view.GestureDetectorCompat
 
 class TitleActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_title)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_UP){
+            loadActionSelection()
+        }
+        return super.onTouchEvent(event)
+    }
+    fun loadActionSelection(){
+        val intent = Intent(this,SelectActionActivity::class.java)
+        startActivity(intent)
     }
 }
