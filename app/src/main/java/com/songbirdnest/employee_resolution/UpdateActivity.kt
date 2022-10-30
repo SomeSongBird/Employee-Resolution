9c85dbba043098b681db6305ca10f67e2d53a452
SomeStupidSongBird <563ftw@gmail.com>
2022-10-29 23:12:38 -0500

    layout of the update activity

--- app/src/main/java/com/songbirdnest/employee_resolution/UpdateActivity.kt
+++ app/src/main/java/com/songbirdnest/employee_resolution/UpdateActivity.kt
@@ -2,8 +2,15 @@ package com.songbirdnest.employee_resolution
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TextView
 
 class UpdateActivity : AppCompatActivity() {
+
+    private lateinit var firstName: TextView
+    private lateinit var lastName: TextView
+    private lateinit var hireDate: TextView
+    private lateinit var errorLine: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update)
